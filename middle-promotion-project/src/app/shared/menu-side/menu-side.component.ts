3ce3c888d367fc2c0fc25e8572d1e7958202fd3c
Valentyn Zozuliuk <valentine.zozuliuk@gmail.com>
2021-12-02 17:56:56 +0200

    Add initial user console

--- middle-promotion-project/src/app/shared/menu-side/menu-side.component.ts
+++ middle-promotion-project/src/app/shared/menu-side/menu-side.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-menu-side',
-  templateUrl: './menu-side.component.html',
-  styleUrls: ['./menu-side.component.scss']
+    selector: 'app-menu-side',
+    templateUrl: './menu-side.component.html',
+    styleUrls: ['./menu-side.component.scss']
 })
 export class MenuSideComponent implements OnInit {
+    @Output() logout = new EventEmitter<void>();
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    logoutHandler() {
+        this.logout.emit();
+    }
 }
--- middle-promotion-project/src/app/user-console/user-console.component.ts
+++ middle-promotion-project/src/app/user-console/user-console.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
-  selector: 'app-user-console',
-  templateUrl: './user-console.component.html',
-  styleUrls: ['./user-console.component.scss']
+    selector: 'app-user-console',
+    templateUrl: './user-console.component.html',
+    styleUrls: ['./user-console.component.scss']
 })
 export class UserConsoleComponent implements OnInit {
 
-  constructor() { }
+    constructor(private auth: AuthService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    onLogoutEvent() {
+        console.log('hadnel');
+        this.auth.logout();
+    }
 
 }
