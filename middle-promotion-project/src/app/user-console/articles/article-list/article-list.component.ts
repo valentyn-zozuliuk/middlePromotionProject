b66fcde390449e4fcd54906c385a6ae0dadd295c
Valentyn Zozuliuk <valentine.zozuliuk@gmail.com>
2021-12-10 11:55:47 +0200

    Minor cleanup

--- middle-promotion-project/src/app/user-console/articles/article-list/article-list.component.ts
+++ middle-promotion-project/src/app/user-console/articles/article-list/article-list.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ClearObservable } from 'src/app/shared/clear-observable/clear-observable';
 
 @Component({
-  selector: 'app-article-list',
-  templateUrl: './article-list.component.html',
-  styleUrls: ['./article-list.component.scss']
+    selector: 'app-article-list',
+    templateUrl: './article-list.component.html',
+    styleUrls: ['./article-list.component.scss']
 })
 export class ArticleListComponent extends ClearObservable implements OnInit {
 
-  constructor() {
-      super();
-   }
+    constructor(private router: Router) {
+        super();
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    addNewArticle() {
+        this.router.navigate(['/user-console/articles/new']);
+    }
 
 }
