62b39128c0050adb42e29a579dfa5d2c39c36f7a
Valentyn Zozuliuk <valentine.zozuliuk@gmail.com>
2021-12-16 17:43:03 +0200

    Add Edit Article initial setup

--- middle-promotion-project/src/app/user-console/articles/article-edit/article-edit.component.ts
+++ middle-promotion-project/src/app/user-console/articles/article-edit/article-edit.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Route, Router } from '@angular/router';
+import { takeUntil } from 'rxjs';
+import { ClearObservable } from 'src/app/shared/clear-observable/clear-observable';
 
 @Component({
-  selector: 'app-article-edit',
-  templateUrl: './article-edit.component.html',
-  styleUrls: ['./article-edit.component.scss']
+    selector: 'app-article-edit',
+    templateUrl: './article-edit.component.html',
+    styleUrls: ['./article-edit.component.scss']
 })
-export class ArticleEditComponent implements OnInit {
+export class ArticleEditComponent extends ClearObservable implements OnInit {
+    editMode: boolean = false;
+    articleForm!: FormGroup;
 
-  constructor() { }
+    constructor(
+        private route: ActivatedRoute,
+        private formBuilder: FormBuilder,
+        private router: Router
+        ) {
+        super();
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.route.params
+            .pipe(
+                takeUntil(this.destroy$)
+            )
+            .subscribe((params: Params) => {
+                this.editMode = params['id'];
+            });
+
+        this.articleForm = this.formBuilder.group({
+            title: ['', Validators.required],
+            text: ['', Validators.required]
+        })
+    }
+
+    onSubmitForm() {
+        console.log('submit');
+    }
+
+    backToDashboard() {
+        this.router.navigate(['/user-console/articles']);
+    }
 
 }
--- middle-promotion-project/src/app/user-console/articles/articles.module.ts
+++ middle-promotion-project/src/app/user-console/articles/articles.module.ts
@@ -11,13 +11,14 @@ import { WeekdayPipe } from './article-list/weather/weekday.pipe';
 import { ArticleComponent } from './article-list/article/article.component';
 import { ArticleDaysAgoPipe } from './article-list/article/article-days-ago.pipe';
 import { TextTruncatePipe } from './article-list/article/text-truncate.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
     {
         path: '', component: ArticlesComponent, children: [
             { path: '', component: ArticleListComponent },
-            { path: ':id', component: ArticleEditComponent },
-            { path: 'new', component: ArticleEditComponent }
+            { path: 'new', component: ArticleEditComponent },
+            { path: ':id', component: ArticleEditComponent }
         ]
     }
 ];
@@ -37,6 +38,7 @@ const routes: Routes = [
     imports: [
         CommonModule,
         SharedModule,
+        ReactiveFormsModule,
         RouterModule.forChild(routes)
     ]
 })
