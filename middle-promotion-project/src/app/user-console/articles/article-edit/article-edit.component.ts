64ef6b49dd039cca9b867a80a1517bbea5f0426c
Valentyn Zozuliuk <valentine.zozuliuk@gmail.com>
2021-12-24 14:25:16 +0200

    Add media dropdown for article add/edit ... ???

--- middle-promotion-project/src/app/user-console/articles/article-edit/article-edit.component.ts
+++ middle-promotion-project/src/app/user-console/articles/article-edit/article-edit.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params, Route, Router } from '@angular/router';
 import { takeUntil } from 'rxjs';
+import { GlobalEventsService } from 'src/app/global-services/global-events.service';
+import { ArticleTypeFilter, ArticleTypesFilter } from 'src/app/model/article.model';
 import { ClearObservable } from 'src/app/shared/clear-observable/clear-observable';
 
 @Component({
@@ -11,12 +13,21 @@ import { ClearObservable } from 'src/app/shared/clear-observable/clear-observabl
 })
 export class ArticleEditComponent extends ClearObservable implements OnInit {
     editMode: boolean = false;
+    selectedTypeFilter!: string | undefined;
     articleForm!: FormGroup;
+    showTypeFilterMenu: boolean = false;
+
+    articleTypeFilters: ArticleTypeFilter[] = [
+        { name: 'Business', code: ArticleTypesFilter.BUSINESS, selected: false },
+        { name: 'Productivity', code: ArticleTypesFilter.PRODUCTIVITY, selected: false },
+        { name: 'Media', code: ArticleTypesFilter.MEDIA, selected: false },
+    ];
 
     constructor(
         private route: ActivatedRoute,
         private formBuilder: FormBuilder,
-        private router: Router
+        private router: Router,
+        private globalEventsService: GlobalEventsService
         ) {
         super();
     }
@@ -30,10 +41,16 @@ export class ArticleEditComponent extends ClearObservable implements OnInit {
                 this.editMode = params['id'];
             });
 
+        this.globalEventsService.globalClickHandler$
+            .pipe(
+                takeUntil(this.destroy$)
+            )
+            .subscribe(() => this.showTypeFilterMenu = false);
+
         this.articleForm = this.formBuilder.group({
             title: ['', Validators.required],
             text: ['', Validators.required]
-        })
+        });
     }
 
     onSubmitForm() {
@@ -48,4 +65,29 @@ export class ArticleEditComponent extends ClearObservable implements OnInit {
 
     }
 
+    toggleType(e: Event) {
+        e.stopPropagation();
+        this.showTypeFilterMenu = !this.showTypeFilterMenu;
+    }
+
+    selectType(type: ArticleTypeFilter, e: Event | null = null, index: number | null = null) {
+        e && e.stopPropagation();
+        index !== null && this.refreshTypeFilter(index);
+        this.getSelectedType();
+        this.showTypeFilterMenu = false;
+    }
+
+    refreshTypeFilter(index: number) {
+        this.articleTypeFilters.forEach((typeFilter: ArticleTypeFilter) => {
+            typeFilter.selected = false;
+        });
+
+        this.articleTypeFilters[index].selected = true;
+    }
+
+    getSelectedType() {
+        this.selectedTypeFilter = this.articleTypeFilters.find((typeFilter: ArticleTypeFilter) => {
+            return typeFilter.selected;
+        })?.name;
+    }
 }
