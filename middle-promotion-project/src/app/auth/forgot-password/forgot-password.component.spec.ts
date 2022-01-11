8ebdc0a0aaeaa34c0d7bba696840d1e5a88f5d20
Valentyn Zozuliuk <valentine.zozuliuk@gmail.com>
2022-01-11 18:51:33 +0200

    Unit test of auth components

--- middle-promotion-project/src/app/auth/forgot-password/forgot-password.component.spec.ts
+++ middle-promotion-project/src/app/auth/forgot-password/forgot-password.component.spec.ts
@@ -2,17 +2,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 import { ForgotPasswordComponent } from './forgot-password.component';
 
 describe('ForgotPasswordComponent', () => {
     let component: ForgotPasswordComponent;
     let fixture: ComponentFixture<ForgotPasswordComponent>;
+    let authService: jasmine.SpyObj<AuthService>;
 
     beforeEach(async () => {
+        const authServiceSpy = jasmine.createSpyObj('AuthService', {
+            resetPassword: of(true)
+        })
         await TestBed.configureTestingModule({
             declarations: [ForgotPasswordComponent],
-            imports: [ReactiveFormsModule, HttpClientModule, RouterTestingModule]
+            imports: [ReactiveFormsModule, HttpClientModule, RouterTestingModule.withRoutes([
+                { path: 'auth/login', redirectTo: '/auth/login'}
+            ])],
+            providers: [{
+                provide: AuthService,
+                useValue: authServiceSpy
+            }]
         })
             .compileComponents();
     });
@@ -20,10 +32,54 @@ describe('ForgotPasswordComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(ForgotPasswordComponent);
         component = fixture.componentInstance;
+        authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('forgot password form is valid if object is valid', () => {
+        component.resetPassForm.patchValue({
+            email: 'email@test.com',
+        });
+
+        fixture.detectChanges();
+
+        expect(component.resetPassForm.valid).toBeTruthy();
+    });
+
+    it('forgot password form is invalid when email is missing', () => {
+        component.resetPassForm.patchValue({
+            email: null
+        });
+
+        fixture.detectChanges();
+
+        expect(component.resetPassForm.valid).toBeFalsy();
+    });
+
+    it('profile form is invalid when email is wrong format', () => {
+        component.resetPassForm.patchValue({
+            email: 'email'
+        });
+
+        fixture.detectChanges();
+
+        expect(component.resetPassForm.valid).toBeFalsy();
+    });
+
+    it('profile form successfully calls the auth service "resetPassword" function if the form is valid', () => {
+        component.resetPassForm.patchValue({
+            email: 'test.email.expected@test.com'
+        });
+
+        component.onSubmitForm();
+
+        fixture.detectChanges();
+
+        expect(component.resetPassForm.valid).toBeTruthy();
+        expect(authService.resetPassword).toHaveBeenCalledTimes(1);
     });
 });
--- middle-promotion-project/src/app/auth/signup/signup.component.spec.ts
+++ middle-promotion-project/src/app/auth/signup/signup.component.spec.ts
@@ -2,17 +2,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { of, throwError } from 'rxjs';
+import { AuthService } from '../auth.service';
 import { SignupComponent } from './signup.component';
 
 describe('SignupComponent', () => {
     let component: SignupComponent;
     let fixture: ComponentFixture<SignupComponent>;
+    let authService: jasmine.SpyObj<AuthService>;
 
     beforeEach(async () => {
+        const authServiceSpy = jasmine.createSpyObj('AuthService', {
+            signup: of(true),
+            googleAuth: of(true),
+            facebookAuth: of(true)
+        })
         await TestBed.configureTestingModule({
             declarations: [SignupComponent],
-            imports: [HttpClientModule, ReactiveFormsModule, RouterTestingModule]
+            imports: [HttpClientModule, ReactiveFormsModule, RouterTestingModule],
+            providers: [
+                {
+                    provide: AuthService,
+                    useValue: authServiceSpy
+                }]
         })
             .compileComponents();
     });
@@ -20,10 +32,221 @@ describe('SignupComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(SignupComponent);
         component = fixture.componentInstance;
+        authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+        spyOn(component.router, 'navigate').and.returnValue(Promise.resolve(true));
+
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('profile form is valid if object is valid', () => {
+        component.profileForm.patchValue({
+            name: 'Test Name',
+            age: 12,
+            email: 'email@test.com',
+            password: '11111111',
+            passwordConfirm: '11111111',
+            acceptTerms: true
+        });
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeTruthy();
+    });
+
+    it('profile form is invalid when name is missing', () => {
+        component.profileForm.patchValue({
+            name: null,
+            age: 12,
+            email: 'email@test.com',
+            password: '11111111',
+            passwordConfirm: '11111111',
+            acceptTerms: true
+        });
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeFalsy();
+    });
+
+    it('profile form is invalid when age is missing', () => {
+        component.profileForm.patchValue({
+            name: 'Test Name',
+            age: null,
+            email: 'email@test.com',
+            password: '11111111',
+            passwordConfirm: '11111111',
+            acceptTerms: true
+        });
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeFalsy();
+    });
+
+    it('profile form is invalid when email is missing', () => {
+        component.profileForm.patchValue({
+            name: 'Test Name',
+            age: 12,
+            email: null,
+            password: '11111111',
+            passwordConfirm: '11111111',
+            acceptTerms: true
+        });
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeFalsy();
+    });
+
+    it('profile form is invalid when email is wrong format', () => {
+        component.profileForm.patchValue({
+            name: 'Test Name',
+            age: 12,
+            email: 'email',
+            password: '11111111',
+            passwordConfirm: '11111111',
+            acceptTerms: true
+        });
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeFalsy();
+    });
+
+    it('profile form is invalid when password is missing', () => {
+        component.profileForm.patchValue({
+            name: 'Test Name',
+            age: 12,
+            email: 'email@test.com',
+            password: null,
+            passwordConfirm: '11111111',
+            acceptTerms: true
+        });
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeFalsy();
+    });
+
+    it('profile form is invalid when password is less than 8 digits', () => {
+        component.profileForm.patchValue({
+            name: 'Test Name',
+            age: 12,
+            email: 'email@test.com',
+            password: '1111',
+            passwordConfirm: '1111',
+            acceptTerms: true
+        });
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeFalsy();
+    });
+
+    it('profile form is invalid when password and confirm Password are different', () => {
+        component.profileForm.patchValue({
+            name: 'Test Name',
+            age: 12,
+            email: 'email@test.com',
+            password: '11111111',
+            passwordConfirm: '222222222',
+            acceptTerms: true
+        });
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeFalsy();
+    });
+
+    it('profile form is invalid when Terms are not accepted', () => {
+        component.profileForm.patchValue({
+            name: 'Test Name',
+            age: 12,
+            email: 'email@test.com',
+            password: '11111111',
+            passwordConfirm: '222222222',
+            acceptTerms: true
+        });
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeFalsy();
+    });
+
+    it('profile form successfully calls the auth service "signup" function if the form is valid', () => {
+        component.profileForm.patchValue({
+            name: 'Test Name',
+            age: 12,
+            email: 'email@test.com',
+            password: '11111111',
+            passwordConfirm: '11111111',
+            acceptTerms: true
+        });
+
+        component.onSubmitForm();
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeTruthy();
+        expect(authService.signup).toHaveBeenCalledTimes(1);
+    });
+
+    it('profile form do not call the auth service "signup" function if the form is invalid', () => {
+        component.profileForm.patchValue({
+            name: null,
+            age: null,
+            email: 'email@test.com',
+            password: '11111111',
+            passwordConfirm: '11111111',
+            acceptTerms: true
+        });
+
+        component.onSubmitForm();
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeFalsy();
+        expect(authService.signup).toHaveBeenCalledTimes(0);
+    });
+
+    it('google signup functionality performs Google Authentication', () => {
+        component.googleSignup();
+
+        fixture.detectChanges();
+
+        expect(authService.googleAuth).toHaveBeenCalledTimes(1);
+    });
+
+    it('facebook signup functionality performs Facebook Authentication', () => {
+        component.facebookSignup();
+
+        fixture.detectChanges();
+
+        expect(authService.facebookAuth).toHaveBeenCalledTimes(1);
+    });
+
+    it('error handling works', () => {
+        component.profileForm.patchValue({
+            name: 'Test Name',
+            age: 12,
+            email: 'email@test.com',
+            password: '11111111',
+            passwordConfirm: '11111111',
+            acceptTerms: true
+        });
+
+        authService.signup.and.returnValue(throwError(() => new Error('Test error')));
+
+        component.onSubmitForm();
+
+        fixture.detectChanges();
+
+        expect(component.profileForm.valid).toBeTruthy();
+        expect(authService.signup).toHaveBeenCalledTimes(1);
+        expect(component.error).toBe('Test error');
     });
 });
