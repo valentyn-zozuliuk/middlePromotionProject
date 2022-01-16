9e52ca98004d43a0f91081ef0b0a560dac46f1cc
Valentyn Zozuliuk <valentine.zozuliuk@gmail.com>
2022-01-16 17:10:06 +0200

    [CYPRESS] setup

--- middle-promotion-project/cypress/integration/login.test.ts
+++ middle-promotion-project/cypress/integration/login.test.ts
@@ -3,4 +3,8 @@ describe('Login page', () => {
     it('should login user', () => {
         expect(true).to.equal(true);
     });
+
+    it('should login user 222', () => {
+        expect(true).to.equal(true);
+    });
 });
